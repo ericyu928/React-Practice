6fcf0a56d290d5aa6740a4d71a2ccc8dd02607cc
ericyu928 <84361126+ericyu928@users.noreply.github.com>
2022-02-08 11:27:13 +0800

    2

--- src/index.js
+++ src/index.js
@@ -4,12 +4,54 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+function formatName(user){
+  return user.firstName+' '+user.lastName;
+}
+const user ={
+  firstName : 'aaa',
+  lastName : 'bbb'
+};
+// const element = (
+//   <div>
+//       <h1>
+//     Hello,{formatName(user)}
+//   </h1>
+//   <h2>
+//     {user.firstName}
+//   </h2>
+//   </div>
+
+// );
+
+// const element =(
+//   <h1 className='greeting'>
+//     test
+//   </h1>
+// )
+const element = React.createElement(
+  'h2',
+  {className:'greeting'},
+  'test123'
+)
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  element,
   document.getElementById('root')
-);
+);  
+
+function tick(){
+  const element =(
+    <div>
+      <h1>test123</h1>
+      <h2>現在是{new Date().toLocaleTimeString()}</h2>
+    </div>
+  )
+  ReactDOM.render(
+    element,
+    document.getElementById('root')
+  )
+}
+setInterval(tick,1000);
+
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
